335988243eb27def61039466e892e4d11cccdca9
Jew68 <pepounlazar@gmail.com>
2021-02-07 14:22:09 -0500

    fixed typos

--- max volume.py
+++ max volume.py
@@ -1,55 +1,56 @@
 import math
 
 width = 1
-hight = 1
-depth = math.floor((500 - hight * width) / (hight + width))
+height = 1
+depth = math.floor((500 - height * width) / (height + width))
 
-volume = width * hight * depth
-surface = 2 * hight * depth + 2 * hight * width + 2 * depth * width
+volume = width * height * depth
+surface = 2 * height * depth + 2 * height * width + 2 * depth * width
 print(f'''We are told to make a box using 1000 cm^2 of material, such that it has the greatest possible volume.
-What lenght should the sides be, assumming they must be integers?
+What length should the sides be, assuming they must be integers?
 
 Starting values:
 width: {width}
-hight: {hight}
+height: {height}
 depth: {depth}
 surface: {surface}
 volume: {volume}
 ''')
 
-while hight < depth:
+while height < depth:
     if width < depth:
         width += 1
-        depth = math.floor((500 - hight * width) / (hight + width))
-        if volume < width * hight * depth:
-            print("w: " + str(width) + ", h: " + str(hight) + ", d: " +
-              str(depth) + ", s: " +
-              str(2 * hight * depth + 2 * hight * width + 2 * depth * width)
-              + ", v: " + str(width * hight * depth) + " - new high")
+        depth = math.floor((500 - height * width) / (height + width))
+        if volume < width * height * depth:
+            print("w: " + str(width) + ", h: " + str(height) + ", d: " +
+                  str(depth) + ", s: " +
+                  str(2 * height * depth + 2 * height * width + 2 * depth * width)
+                  + ", v: " + str(width * height * depth) + " - new max")
             max_width = width
-            max_hight = hight
+            max_height = height
             max_depth = depth
             max_surface = surface
-            volume = width * hight * depth
+            volume = width * height * depth
         else:
-            print("w: " + str(width) + ", h: " + str(hight) + ", d: " +
-              str(depth) + ", s: " +
-              str(2 * hight * depth + 2 * hight * width + 2 * depth * width)
-              + ", v: " + str(width * hight * depth))
+            print("w: " + str(width) + ", h: " + str(height) + ", d: " +
+                  str(depth) + ", s: " +
+                  str(2 * height * depth + 2 * height * width + 2 * depth * width)
+                  + ", v: " + str(width * height * depth))
     else:
-        width = hight
-        hight += 1
-        depth = math.floor((500 - hight * width) / (hight + width))
+        width = height
+        height += 1
+        depth = math.floor((500 - height * width) / (height + width))
 
 print(f'''
 width: {max_width}
-hight: {max_hight}
+height: {max_height}
 depth: {max_depth}
 surface: {max_surface}
 volume: {volume}
-The box with the greatest volume will have sides the are {max_width}, {max_hight}, and {max_depth} cm long.
+The box with the greatest volume will have sides the are {max_width}, {max_height}, and {max_depth} cm long.
 
-You can imput 'boxcalc(#, #, #)' to see the surface and volume of any other box''')
+You can input 'boxcalc(#, #, #)' to see the surface and volume of any other box''')
+
 
 def boxcalc(x, y, z):
     print("surface: " + str(2 * x * y + 2 * x * z + 2 * y * z))
